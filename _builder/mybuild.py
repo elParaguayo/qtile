a7294048214e770a8c80cf95a5fc5535cd68d3f2
elParaguayo <elparaguayocode@gmail.com>
2023-09-04 22:30:34 +0100

    debug

--- _builder/mybuild.py
+++ _builder/mybuild.py
@@ -1,8 +1,11 @@
 from setuptools import build_meta as _orig
 from setuptools.build_meta import *
+import sys
 
 
 def get_requires_for_build_wheel(config_settings=None):
     print("ADDING WLROOTS TP WHEEL REQUIREMENTS")
     print(config_settings)
+    print(_orig._BACKEND.__dict__)
+    print(sys.argv)
     return _orig.get_requires_for_build_wheel(config_settings) + ["pywlroots"]
